c2aaf61c3af97653c5767b8a737c1d7e9f30c3c2
Stefano Colucci <stefano.colucci@ubidata.com>
2022-06-12 09:53:10 +0200

    FEAT : adds nuget packages for postgresql with EF , adds  DbContext class

---
+++ src/Acme.Infrastructure/ProductContext.cs
@@ -0,0 +1,29 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Acme.Domain.AggregatesModel.ProductAggregate;
+using Acme.Domain.AggregatesModel.WarehouseAggregate;
+using Acme.Domain.Seedwork;
+using Microsoft.EntityFrameworkCore;
+
+namespace Acme.Infrastructure;
+
+public class ProductContext : DbContext, IUnitOfWork
+{
+
+   public DbSet<Product> Products { get; set; }
+
+   public DbSet<Warehouse> Warehouses { get; set; }
+
+   public DbSet<ProductPicture> ProductPictures { get; set; }
+
+   public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+   {
+
+   }
+
+
+   public Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+   {
+      throw new System.NotImplementedException();
+   }
+}
